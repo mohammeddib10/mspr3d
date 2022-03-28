c2501b4d5d85decd97d17308b2adcd2a5a3c49f6
mohammed <mohammeddib85@gmail.com>
2022-03-28 11:17:33 +0200

    first commit

---
+++ Assets/Deplacement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Deplacement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * 60);
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * 60);
+    }
+}
---
+++ Assets/src/sample/data/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Boomlagoon.JSON;
+using Salesforce;
+
+public class Account : SalesforceRecord {
+
+    public const string BASE_QUERY = "SELECT Name, Phone, Fax FROM Account";
+
+    public string name { get; set; }
+    public string phone { get; set; }
+    public string fax { get; set; }
+
+    public Account() {}
+
+    public Account(string name, string phone, string fax) {
+        this.name = name;
+        this.phone = phone;
+        this.fax = fax;
+    }
+
+    public override string getSObjectName() {
+        return "Account";
+    }
+
+    public override JSONObject toJson() {
+        JSONObject record = base.toJson();
+        record.Add("Name", name);
+        record.Add("Phone", phone);
+        record.Add("Fax", fax);
+        return record;
+    }
+
+    public override void parseFromJson(JSONObject jsonObject) {
+        base.parseFromJson(jsonObject);
+        name = jsonObject.GetString("Name");
+        phone = jsonObject.GetString("Phone");
+        fax = jsonObject.GetString("Fax");
+    }
+}
