cb6e24744e0bcb6553cccfb06179f434222a570b
mohammed dib <mohammed.dib@kheoos.com>
2023-02-21 20:59:54 +0100

    commit deplacement

--- Assets/Deplacement.cs
+++ Assets/Deplacement.cs
@@ -13,5 +13,9 @@ public class Deplacement : MonoBehaviour
     {
         transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * 60);
         transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * 60);
+        
+        
+        
+        
     }
 }
